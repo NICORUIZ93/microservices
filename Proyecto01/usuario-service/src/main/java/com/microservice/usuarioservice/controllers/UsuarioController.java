79df1b73c381b4669415433751aee0df8bb2dc8a
Nicolás Ruiz <NICOLASENRIQUERUIZ93@HOTMAIL.COM>
2023-07-11 14:27:36 -0500

    Add circuitbraker

--- Proyecto01/usuario-service/src/main/java/com/microservice/usuarioservice/controllers/UsuarioController.java
+++ Proyecto01/usuario-service/src/main/java/com/microservice/usuarioservice/controllers/UsuarioController.java
@@ -2,12 +2,15 @@ package com.microservice.usuarioservice.controllers;
 
 import com.microservice.usuarioservice.entities.Usuario;
 import com.microservice.usuarioservice.service.UsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -24,15 +27,28 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> usuarios = usuarioService.getAllUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
     @GetMapping("/{usuarioId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<Usuario> obtenerUsuario(@PathVariable String usuarioId) {
         Usuario usuario = usuarioService.getUsuario(usuarioId);
         return ResponseEntity.ok(usuario);
     }
 
-    @GetMapping
-    public ResponseEntity<List<Usuario>> listarUsuarios() {
-        List<Usuario> usuarios = usuarioService.getAllUsuarios();
-        return ResponseEntity.ok(usuarios);
+    public ResponseEntity<Usuario> ratingHotelFallback(String usuarioId, Exception exception) {
+        log.info("El respaldo se ejecuto por que el servicio esta inactivo: ", exception.getMessage());
+        Usuario usuario = Usuario.builder()
+                .email("root@root.com")
+                .nombre("root")
+                .informacion("Este usuario se crea por defecto cuando un servicio se cae")
+                .usuarioId("1234")
+                .build();
+
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 }
